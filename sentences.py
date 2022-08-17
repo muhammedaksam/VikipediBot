630c912a22a3fc030d8a1591410652f8009c9411
Muhammed Mustafa Akşam <mmustafaaksam@trakya.edu.tr>
2022-08-17 16:27:45 +0300

    Update sentences.py

--- sentences.py
+++ sentences.py
@@ -3,8 +3,8 @@ from datetime import datetime
 # Reddit markdown is used in this string
 comment_reply = f"\n\n\n\n*Bu yorum (bir bot tarafından) otomatik olarak bırakıldı." \
                 f" Bunu doğru anlamadıysam, bana kızmayın, hala öğreniyorum!*\n\n" \
-                f"[^(listeden çık)](https://www.reddit.com/r/VikipediBot/comments/wpslrb/listeden_%C3%A7%C4%B1kma_ba%C5%9Fl%C4%B1%C4%9F%C4%B1/)" \
-                f" ^(|) [^(r/VikipediBot)](https://www.reddit.com/r/VikipediBot)" \
+                f"[^(listeden çık)](https://np.reddit.com/r/VikipediBot/comments/wpslrb/listeden_%C3%A7%C4%B1kma_ba%C5%9Fl%C4%B1%C4%9F%C4%B1/)" \
+                f" ^(|) [^r/VikipediBot" \
                 f" ^(|) [^(GitHub)](https://github.com/muhammedaksam/VikipediBot)"
 
 
